b843677b9a6a4af6222eda7503bf24784530a858
fred-dev <vendeur69400@gmail.com>
2022-04-22 17:53:02 +0200

    texte dans les p

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-let text= document.getElementsByClassName ("testHtml");
 
 
-text.innerHTML="bonjour";
+//météo
+fetch("https://meteofrance.com/")
 
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
 
  
