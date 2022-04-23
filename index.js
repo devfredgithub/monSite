ef5d96bb51ce85b0656bd7a7513efd22f2df1232
fred-dev <vendeur69400@gmail.com>
2022-04-24 01:29:36 +0200

    client

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 
 
-//météo
-fetch("https://meteofrance.com/")
-
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(value) {
-    console.log(value);
-  })
-  .catch(function(err) {
-    // Une erreur est survenue
-  });
-
  
